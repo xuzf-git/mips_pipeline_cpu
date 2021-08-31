160afd9b9379af093875316321f41e625460d894
xuzf <xuzhengfei-email@qq.com>
2021-08-31 14:35:34 +0800

    add mips_cpu

--- project/mips_pipeline_cpu/mips_pipeline_cpu.srcs/sources_1/new/mips_cpu.v
+++ project/mips_pipeline_cpu/mips_pipeline_cpu.srcs/sources_1/new/mips_cpu.v
@@ -11,7 +11,7 @@
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
-// Description: 
+// Description: 实例化各个模块，创建流水线CPU
 // 
 // Dependencies: 
 // 
@@ -35,10 +35,16 @@ module mips_cpu(
     output  wire[`RegBus]    ram_data_o
     );
 
+    // 连接 PC 模块和 IF_ID 模块
+    wire[`RegBus]       if_id_pc_i;
+
     // 连接 PC 模块和 ID 模块
-    wire[`RegBus]       pc;
     wire                branch_flag;
     wire[`RegBus]       branch_target;
+
+    // 连接 IF_ID 模块和 ID 模块
+    wire[`RegBus]       if_id_pc_o;
+    wire[`RegBus]       if_id_inst_o;
 
     // 连接 ID 模块和 Regfile 模块
     wire                reg_re1;
@@ -48,26 +54,47 @@ module mips_cpu(
     wire[`RegAddrBus]   reg_raddr1;
     wire[`RegAddrBus]   reg_raddr2;
 
-    // 连接 ID 模块和 EX 模块
-    wire[`AluSelBus]    alu_sel;
-    wire[`RegBus]       alu_opnd1;
-    wire[`RegBus]       alu_opnd2;
-    wire                reg_we;
-    wire[`RegAddrBus]   reg_waddr;
-    wire[`RegBus]       id_ex_inst;
-    
-    // 连接 EX 模块和 MEM 模块
-    wire                ex_we;
-    wire[`RegAddrBus]   ex_waddr;
-    wire[`RegBus]       ex_wdata;
-    wire[`AluSelBus]    ex_alu_sel;
-    wire[`RegBus]       ex_ram_addr;
-    wire[`RegBus]       ex_reg_rt;
+    // 连接 ID 模块和 ID_EX 模块
+    wire[`AluSelBus]    id_ex_alu_sel_i;
+    wire[`RegBus]       id_ex_alu_opnd1_i;
+    wire[`RegBus]       id_ex_alu_opnd2_i;
+    wire                id_ex_reg_we_i;
+    wire[`RegAddrBus]   id_ex_reg_waddr_i;
+    wire[`RegBus]       id_ex_inst_i;
 
-    // 连接 MEM 模块和 WB 模块
-    wire                mem_we;
-    wire[`RegAddrBus]   mem_waddr;
-    wire[`RegBus]       mem_wdata;
+    // 连接 ID_EX 模块和 EX 模块
+    wire[`AluSelBus]    id_ex_alu_sel_o;
+    wire[`RegBus]       id_ex_alu_opnd1_o;
+    wire[`RegBus]       id_ex_alu_opnd2_o;
+    wire                id_ex_reg_we_o;
+    wire[`RegAddrBus]   id_ex_reg_waddr_o;
+    wire[`RegBus]       id_ex_inst_o;
+    
+    // 连接 EX 模块和 EX_MEM 模块
+    wire                ex_mem_we_i;
+    wire[`RegAddrBus]   ex_mem_waddr_i;
+    wire[`RegBus]       ex_mem_wdata_i;
+    wire[`AluSelBus]    ex_mem_alu_sel_i;
+    wire[`RegBus]       ex_mem_ram_addr_i;
+    wire[`RegBus]       ex_mem_reg_rt_i;
+    
+    // 连接 EX_MEM 模块和 MEM 模块
+    wire                ex_mem_we_o;
+    wire[`RegAddrBus]   ex_mem_waddr_o;
+    wire[`RegBus]       ex_mem_wdata_o;
+    wire[`AluSelBus]    ex_mem_alu_sel_o;
+    wire[`RegBus]       ex_mem_ram_addr_o;
+    wire[`RegBus]       ex_mem_reg_rt_o;
+
+    // 连接 MEM 模块和 MEM_WB 模块
+    wire                mem_wb_we_i;
+    wire[`RegAddrBus]   mem_wb_waddr_i;
+    wire[`RegBus]       mem_wb_wdata_i;
+
+    // 连接 MEM_WB 模块和 WB 模块
+    wire                mem_wb_we_o;
+    wire[`RegAddrBus]   mem_wb_waddr_o;
+    wire[`RegBus]       mem_wb_wdata_o;
 
     // 连接 WB 模块和 Regfile 模块
     wire                wb_we;
@@ -82,19 +109,29 @@ module mips_cpu(
         .branch_flag_i(branch_flag),
         .branch_target_i(branch_target),
         // 输出到指令存储器 ROM 的信息
-        .pc_reg(pc),
+        .pc_reg(if_id_pc_i),
         .ce(rom_re_o)
     );
 
     assign rom_raddr_o = pc;
+
+    // 实例化 IF_ID 模块
+    if_id if_id_real(
+        .clk(clk),
+        .rst(rst),
+        .if_pc_i(if_id_pc_i),
+        .if_inst_i(rom_rdata_i),
+        .id_pc_o(if_id_pc_o),
+        .id_inst_o(if_id_inst_o)
+    );
 
     // 实例化 ID
     id id_real(
         .rst(rst),  
 
         // 来自 PC 模块的输入
-        .inst_i(rom_rdata_i),
-        .pc_i(pc),
+        .inst_i(if_id_inst_o),
+        .pc_i(if_id_pc_o),
 
         // 输出到 PC 模块的转移信息
         .branch_flag_o(branch_flag),
@@ -111,12 +148,12 @@ module mips_cpu(
         .reg_raddr2_o(reg_raddr2),
 
         // 输出到 EX 模块的信息
-        .alu_sel_o(alu_sel),
-        .alu_opnd1_o(alu_opnd1),
-        .alu_opnd2_o(alu_opnd2),
-        .reg_waddr_o(reg_waddr),
-        .reg_we_o(reg_we),
-        .inst_o(id_ex_inst)
+        .alu_sel_o(id_ex_alu_sel_i),
+        .alu_opnd1_o(id_ex_alu_opnd1_i),
+        .alu_opnd2_o(id_ex_alu_opnd2_i),
+        .reg_waddr_o(id_ex_reg_waddr_i),
+        .reg_we_o(id_ex_reg_we_i),
+        .inst_o(id_ex_inst_i)
     );
 
     // 实例化 Regfile
@@ -140,25 +177,62 @@ module mips_cpu(
         .rdata2_o(reg_rdata2)
     );
 
+    // 实例化 ID_EX 模块
+    id_ex id_ex_real(
+        .clk(clk),
+        .rst(rst),
+        .id_alu_sel_i(id_ex_alu_sel_i),
+        .id_alu_opnd1_i(id_ex_alu_opnd1_i),
+        .id_alu_opnd2_i(id_ex_alu_opnd2_i),
+        .id_reg_waddr_i(id_ex_reg_waddr_i),
+        .id_reg_we_i(id_ex_reg_we_i),
+        .id_inst_i(id_ex_inst_i),
+        .ex_alu_sel_o(id_ex_alu_sel_o),
+        .ex_alu_opnd1_o(id_ex_alu_opnd1_o),
+        .ex_alu_opnd2_o(id_ex_alu_opnd2_o),
+        .ex_reg_waddr_o(id_ex_reg_waddr_o),
+        .ex_reg_we_o(id_ex_reg_we_o),
+        .ex_inst_o(id_ex_inst_o)
+    );
+
     // 实例化 EX 模块
     ex ex_real(
         .rst(rst),
 
         // 从 ID 模块传来的信息
-        .inst_i(id_ex_inst),
-        .alu_sel_i(alu_sel),
-        .alu_opnd1_i(alu_opnd1),
-        .alu_opnd2_i(alu_opnd2),
-        .reg_waddr_i(reg_waddr),
-        .reg_we_i(reg_we),
+        .inst_i(id_ex_inst_o),
+        .alu_sel_i(id_ex_alu_sel_o),
+        .alu_opnd1_i(id_ex_alu_opnd1_o),
+        .alu_opnd2_i(id_ex_alu_opnd2_o),
+        .reg_waddr_i(id_ex_reg_waddr_o),
+        .reg_we_i(id_ex_reg_we_o),
 
         // 输出到 MEM 模块的信息
-        .reg_waddr_o(ex_waddr),
-        .reg_we_o(ex_we),
-        .reg_wdata_o(ex_wdata),
-        .alu_sel_o(ex_alu_sel),
-        .ram_addr_o(ex_ram_addr),
-        .reg_rt_o(ex_reg_rt)
+        .reg_waddr_o(ex_mem_waddr_i),
+        .reg_we_o(ex_mem_we_i),
+        .reg_wdata_o(ex_mem_wdata_i),
+        .alu_sel_o(ex_mem_alu_sel_i),
+        .ram_addr_o(ex_mem_ram_addr_i),
+        .reg_rt_o(ex_mem_reg_rt_i)
+    );
+
+    // 实例化 EX_MEM 模块
+    ex_mem ex_mem_real(
+        .clk(clk),
+        .rst(rst),
+        .ex_waddr_i(ex_mem_waddr_i),
+        .ex_we_i(ex_mem_we_i),
+        .ex_wdata_i(ex_mem_wdata_i),
+        .ex_alu_sel_i(ex_mem_alu_sel_i),
+        .ex_ram_addr_i(ex_mem_ram_addr_i),
+        .ex_reg_rt_i(ex_mem_reg_rt_i),
+
+        .mem_waddr_o(ex_mem_waddr_o),
+        .mem_we_o(ex_mem_we_o),
+        .mem_wdata_o(ex_mem_wdata_o),
+        .mem_alu_sel_o(ex_mem_alu_sel_o),
+        .mem_ram_addr_o(ex_mem_ram_addr_o),
+        .mem_reg_rt_o(ex_mem_reg_rt_o)
     );
 
     // 实例化 MEM 模块
@@ -166,20 +240,20 @@ module mips_cpu(
         .rst(rst),
 
         // 从 EX 模块传来的信息
-        .ex_waddr_i(ex_waddr),
-        .ex_we_i(ex_we),
-        .ex_wdata_i(ex_wdata),
-        .ex_alu_sel_i(ex_alu_sel),
-        .ex_ram_addr_i(ex_ram_addr),
-        .ex_reg_rt_i(ex_reg_rt),
+        .ex_waddr_i(ex_mem_waddr_o),
+        .ex_we_i(ex_mem_we_o),
+        .ex_wdata_i(ex_mem_wdata_o),
+        .ex_alu_sel_i(ex_mem_alu_sel_o),
+        .ex_ram_addr_i(ex_mem_ram_addr_o),
+        .ex_reg_rt_i(ex_mem_reg_rt_o),
 
         // 从数据存储器传来的信号
         .ram_data_i(ram_data_i),
         
         // 输出到 WB 模块的信息
-        .mem_waddr_o(mem_waddr),
-        .mem_we_o(mem_we),
-        .mem_wdata_o(mem_wdata),
+        .mem_waddr_o(mem_wb_waddr_i),
+        .mem_we_o(mem_wb_we_i),
+        .mem_wdata_o(mem_wb_wdata_i),
 
         // 输出到数据存储器的信号
         .mem_ram_addr_o(ram_addr_o),
@@ -187,14 +261,26 @@ module mips_cpu(
         .mem_ram_data_o(ram_data_o)
     );
 
+    // 实例化 MEM_WB 模块
+    mem_wb mem_wb_real(
+        .clk(clk),
+        .rst(rst),
+        .mem_waddr_i(mem_wb_waddr_i),
+        .mem_we_i(mem_wb_we_i),
+        .mem_wdata_i(mem_wb_wdata_i),
+        .wb_waddr_o(mem_wb_waddr_o),
+        .wb_we_o(mem_wb_we_o),
+        .wb_wdata_o(mem_wb_wdata_o),
+    ); 
+
     // 实例化 WB 模块
     wb wb_real(
         .rst(rst),
 
         // 从 MEM 模块传来的信息
-        .mem_waddr_i(mem_waddr),
-        .mem_we_i(mem_we),
-        .mem_wdata_i(mem_wdata),
+        .mem_waddr_i(mem_wb_waddr_o),
+        .mem_we_i(mem_wb_we_o),
+        .mem_wdata_i(mem_wb_wdata_o),
 
         // 输出到 Regfile 模块的信息
         .wb_waddr_o(wb_waddr),
